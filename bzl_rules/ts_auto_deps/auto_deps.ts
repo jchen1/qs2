2c332e4f10a9c047378cb28105476b2640d049ec
Jeff Chen <hello@jeff.yt>
2019-11-12 20:49:53 -0800

    implement tsLibrary()

--- bzl_rules/ts_auto_deps/auto_deps.ts
+++ bzl_rules/ts_auto_deps/auto_deps.ts
@@ -1,5 +1,7 @@
 import ts from 'typescript';
 
+// ------------------ TYPESCRIPT PARSING ------------------
+
 function getImportsForNode(node: ts.Node, checker: ts.TypeChecker, imports: string[]) {
   if (ts.isImportDeclaration(node)) {
     const lib = node.moduleSpecifier.getFullText().trim().replace(/(\"|\')/gi, "");
@@ -12,21 +14,46 @@ function getImportsForNode(node: ts.Node, checker: ts.TypeChecker, imports: stri
   ts.forEachChild(node, n => getImportsForNode(n, checker, imports));
 }
 
-export function getImports(sourceFile: ts.SourceFile, checker: ts.TypeChecker): string[] {
+function getImports(sourceFile: ts.SourceFile, checker: ts.TypeChecker): string[] {
   const imports: string[] = [];
   getImportsForNode(sourceFile, checker, imports);
   return imports;
 }
 
-const files = process.argv.slice(2);
-const program = ts.createProgram(files, {}); // todo es2017, ???
-const checker = program.getTypeChecker();
-const importsByFile: { [key:string]: string[] } = {};
+// ------------------ BAZEL RULES ------------------
 
-files.forEach(f => {
-  const source = program.getSourceFile(f)!;
-  // const source = ts.createSourceFile(f, readFileSync(f).toString(), ts.ScriptTarget.ES2017, true);
-  importsByFile[f] = getImports(source, checker);
-});
+function importToBazelDeps(fileName: string, imp: string): string {
+  // todo...
+  return `//:${imp}`;
+}
 
-console.log(importsByFile);
+function tsLibrary(fileName: string, deps: string[]): string {
+  const srcName = `${fileName.split("/").reverse()[0]}`;
+  const depsString = deps.map(d => `    "${d}",`).join("\n");
+
+  return `
+ts_library(
+  name = "${srcName.replace(/\.ts$/gi, "")}_lib",
+  srcs = ["${srcName}"],
+  deps = [
+${depsString}
+  ],
+)
+  `
+}
+
+// ------------------ PUBLIC API ------------------
+
+export function autoDeps() {
+  const files = process.argv.slice(2);
+  const program = ts.createProgram(files, {}); // todo es2017, ???
+  const checker = program.getTypeChecker();
+
+  files.forEach(f => {
+    const source = program.getSourceFile(f)!;
+    const imports = getImports(source, checker);
+    // const rules = imports.map(i => )
+  });
+}
+
+autoDeps();
