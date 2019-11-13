e5e890811ac58ed9feb1f5db5d40c99eca415ec7
Jeff Chen <hello@jeff.yt>
2019-11-12 16:23:00 -0800

    first prettier attempt

--- src/ts/context.ts
+++ src/ts/context.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import { PubSub } from 'graphql-subscriptions'
-import knex from 'knex'
+import express from "express";
+import { PubSub } from "graphql-subscriptions";
+import knex from "knex";
 
 export interface GraphQLContext {
-  pubsub: PubSub
-  req: express.Request
-  db: knex<any, unknown[]>
+  pubsub: PubSub;
+  req: express.Request;
+  db: knex<any, unknown[]>;
 }
